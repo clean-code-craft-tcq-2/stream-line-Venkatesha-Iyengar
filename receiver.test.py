b8c065f6aa0af0df12c930abc8adc745bad10f7d
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-18 13:41:30 +0200

    Update receiver.test.py

--- receiver.test.py
+++ receiver.test.py
@@ -1,1 +1,8 @@
+import unittest
+from receiver import *
 
+class tdd_receiver_test(unittest.TestCase):
+    def test_receiver(self):
+        ReadDataFromConsoleAndPrintStatistics()
+    
+unittest.main()

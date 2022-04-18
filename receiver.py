e3ebef7763d164fc7549991b049294c424062a03
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-18 11:16:35 +0200

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,5 +1,11 @@
 import sys
 
+BatteryParameters = ["Current Readings in milliAmps", "Temperature Readings in Farenheit"]
+
+def ReadBatteryParameterDataFromConsole():
+    LinesRead = sys.stdin.readlines()
+    return LinesRead  
+  
 def ReadDataFromConsoleAndPrintStatistics():
   DataReadFromConsole = ReadBatteryParameterDataFromConsole()
   for Parameter in BatteryParameters:

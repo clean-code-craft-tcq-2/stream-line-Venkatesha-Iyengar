747bdf748ef73a8670400ac4a2e92552bdec002a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-18 11:09:05 +0200

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,1 +1,8 @@
+import sys
 
+def ReadDataFromConsoleAndPrintStatistics():
+  DataReadFromConsole = ReadBatteryParameterDataFromConsole()
+  for Parameter in BatteryParameters:
+    RangeOfDataRead = FindRangeFromDataRead(DataReadFromConsole)
+    MovingAvgOfDataRead = FindMovingAverageOfDataRead(DataReadFromConsole)
+    printStatisticsOnConsole(Parameter,RangeOfDataRead,MovingAvgOfDataRead)

c18f986a6d93178f76df45302497fc4e3808724f
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-18 11:58:00 +0200

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -5,7 +5,12 @@ BatteryParameters = ["Current Readings in milliAmps", "Temperature Readings in F
 def ReadBatteryParameterDataFromConsole():
     LinesRead = sys.stdin.readlines()
     return LinesRead  
-  
+
+def FindRangeFromDataRead(Data):
+    minimumValueAmongData = min(Data,default="NULL")
+    minimumValueAmongData = max(Data,default="NULL")
+    return {'Minimum value of the parameter': MinimumValueAmongData, 'Maximum value of the parameter': MaximumValueAmongData}
+
 def ReadDataFromConsoleAndPrintStatistics():
   DataReadFromConsole = ReadBatteryParameterDataFromConsole()
   for Parameter in BatteryParameters:

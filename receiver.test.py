680e52eb6873f8b68124149ae26e562ff4d62006
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-18 16:08:11 +0200

    Update receiver.test.py

--- receiver.test.py
+++ receiver.test.py
@@ -2,10 +2,15 @@ import unittest
 from receiver import *
 
 class tdd_receiver_test(unittest.TestCase):
-    def test_receiver(self):
+    def Test_Range_Receiver(self):
+        AverageDataSize = 5
+        DataReadFromConsole = [10,20,30,40,0,21,47,99,12]
+        RangeOfDataRead = FindRangeFromDataRead(Data)
+        self.assertEqual(RangeOfDataRead,('Minimum value of the parameter': 0, 'Maximum value of the parameter': 99))
+        
+    def Test_MovingAverage_Receiver(self):
         AverageDataSize = 5
         DataReadFromConsole = [10,20,30,40,0]
-        ReadDataFromConsoleAndPrintStatistics(AverageDataSize)
         MovingAvergae = FindMovingAverageOfDataRead(DataReadFromConsole,AverageDataSize)
         self.assertEqual(MovingAvergae,[20])
     

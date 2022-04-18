8da6f4b28aa48efad7654fa539527dcdea9bc744
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-18 15:44:06 +0200

    Update receiver.test.py

--- receiver.test.py
+++ receiver.test.py
@@ -4,6 +4,9 @@ from receiver import *
 class tdd_receiver_test(unittest.TestCase):
     def test_receiver(self):
         AverageDataSize = 5
+        DataReadFromConsole = [10,20,30,40,0]
         ReadDataFromConsoleAndPrintStatistics(AverageDataSize)
+        MovingAvergae = FindMovingAverageOfDataRead(DataReadFromConsole,AverageDataSize):
+        self.assertEqual(MovingAvergae,30)
     
 unittest.main()
